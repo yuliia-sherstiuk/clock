d02c42220f2eb7744203e07160aee31315d8e5da
christine chemali <christine.chemali@laplateforme.io>
2025-01-06 22:55:57 +0100

    set time, set alarm, check alarm function

--- clock.py
+++ clock.py
@@ -25,4 +25,21 @@ def update_time():
         if current_time[0] == 24:
             current_time = (0 , 0 , 0)
 
-update_time()
+# Function that set the current time
+def set_time(new_time):
+    global current_time
+    current_time = new_time
+    display_time(current_time)
+
+# Function that set the alarm
+def set_alarm(new_alarm_time):
+    global alarm_time
+    alarm_time = new_alarm_time
+    print (f"Alarm time set for {alarm_time[0]:02} : {alarm_time[1]:02} : {alarm_time[2]:02}")
+
+# Function that check if the alarm should ring
+def check_alarm():
+    if current_time == alarm_time:
+        print ("DRING DRING !!! Time to wake up !!!")
+
+
